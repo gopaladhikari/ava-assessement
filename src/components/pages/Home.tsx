1a63f36a9bd2839e4b746d1090e60bd076e6a1ee
gopaladhikari <gopuadks@gmail.com>
2024-08-14 22:32:15 +0545

    redirect from / to /feed if logged in

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { site } from "../../config/constants";
 import { MaxWidthWrapper } from "../partials/MaxWidthWrapper";
 import { Metadata } from "../partials/Metadata";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Home() {
+	const auth = useAuth();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!auth?.isLoading && auth?.user) navigate("/feed");
+	}, [auth?.isLoading]);
+
 	return (
 		<main>
 			<Metadata title={site.title} description={site.description} />
